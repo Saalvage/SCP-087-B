01d7d65f0a78caf1018b0dc0ff73f6a2c721609c
Salvage <29021710+Saalvage@users.noreply.github.com>
2025-06-10 19:18:20 +0200

    Improve DreamFilter by relying on `Control`s

--- Common/DreamFilter.cs
+++ Common/DreamFilter.cs
@@ -3,15 +3,7 @@ using Godot;
 public partial class DreamFilter : FullSizeViewport {
     private double _acc;
 
-    public required PlayerController Player;
-    [Export] public required Sprite2D Child;
-
     [Export] public float UpdateInterval = 1f / 60f;
-
-    public override void _Ready() {
-        base._Ready();
-        Child.Scale = Size / new Vector2(512, 512);
-    }
 
     public override void _Process(double delta) {
         _acc += delta;
