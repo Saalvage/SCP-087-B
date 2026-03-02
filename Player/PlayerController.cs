5cec13ef12e7535e0b83a257e4452630814b7044
Salvage <29021710+Saalvage@users.noreply.github.com>
2026-03-02 20:40:44 +0100

    Fix formatting again

--- Player/PlayerController.cs
+++ Player/PlayerController.cs
@@ -4,16 +4,18 @@ using Godot;
 [SceneGlobal]
 public partial class PlayerController : CharacterControllerBase {
     [Export] public required AudioStream Step;
+    
     [Export] public required AnimationPlayer Animations;
     [Export] public required Node3D CameraRail;
     [Export] public required Camera3D Camera;
     [Export] public float ForwardSpeed = 1.2f;
     [Export] public float BackwardSpeed = 0.9f;
     [Export] public float SidewardsSpeed = 0.48f;
+    
     [Export] public byte Brightness = 40;
     // Originally 2.5f, adjusted to account for radial fog.
-    [Export] public float FogFar = 3f;
     [Export] public float FogNear = 1f;
+    [Export] public float FogFar = 3f;
     
     [Export] private float MouseSensitivity = 0.2f;
     [Export] private float ControllerSensitivity = 300f;
@@ -23,36 +25,34 @@ public partial class PlayerController : CharacterControllerBase {
         get;
         set {
             field = value;
+            
             if (value == 0f) {
                 Helpers.SetBrightness(Brightness);
                 return;
             }
+            
             var subtract = float.Lerp(0f, 90f / 255f, value);
             Helpers.SetBrightness(Color.Color8(255, 100, 100) - new Color(subtract, subtract, subtract));
         }
     }
 
     private bool _isDead;
-    
-    private static readonly Vector3 HeadBobInterval = new Vector3(80f, 40f, 1f) / 60;
+
+    private static readonly Vector3 HeadBobInterval = new Vector3(80f, 40f, 1f) / 60f;
     private static readonly Vector3 HeadBobOffset = new(0.08f, 0.1f, 0f);
     private float _headBobAcc;
-    
+
     public int Floor => Helpers.GetFloor(GlobalPosition - new Vector3(0f, 0.5f, 0f));
 
     public override void _Ready() {
         Input.MouseMode = Input.MouseModeEnum.Captured;
         SetFogRange(FogNear, FogFar);
         Helpers.SetBrightness(Brightness);
-
-        var r = CameraRail.Rotation;
     }
 
     public override void _Input(InputEvent @event) {
         if (@event is InputEventMouseMotion mm) {
-            var invertedY = -mm.Relative.Y;
-            var delta = new Vector2(mm.Relative.X, -invertedY) * MouseSensitivity;
-            HandleLook(delta);
+            HandleLook(mm.Relative * MouseSensitivity);
         }
     }
 
@@ -67,33 +67,18 @@ public partial class PlayerController : CharacterControllerBase {
     }
 
     private void HandleLook(Vector2 lookDelta) {
-        var r = CameraRail.RotationDegrees;
-        float minmaxpitch = 89.999f; // this being exactily 90 would mess up the movement when looking at that angle
-        var minPitch = -minmaxpitch;
-        var maxPitch = minmaxpitch;
-
-        // Apply lookDelta
-        r.X -= lookDelta.Y;
-        r.Y += -lookDelta.X;
-
-        r.X = Mathf.Clamp(r.X, minPitch, maxPitch);
-
-        // Apply rotation
-        CameraRail.RotationDegrees = new Vector3(r.X, r.Y, 0f);
+        var rot = CameraRail.RotationDegrees - new Vector3(lookDelta.Y, lookDelta.X, 0);
+        CameraRail.RotationDegrees = rot with { X = float.Clamp(rot.X, -89.9f, 89.9f) };
     }
 
     public override void _PhysicsProcess(double deltaD) {
-        var inputDir = _isDead
-            ? Vector2.Zero
-            : new(
-                SidewardsSpeed *
-                (Input.GetActionStrength(Actions.MoveRight) - Input.GetActionStrength(Actions.MoveLeft)),
-                (Input.GetActionStrength(Actions.MoveBackward) != 0) ^
-                (Input.GetActionStrength(Actions.MoveForward) != 0)
-                    ? -ForwardSpeed * Input.GetActionStrength(Actions.MoveForward) +
-                      BackwardSpeed * Input.GetActionStrength(Actions.MoveBackward)
-                    : 0
-            );
+        var inputDir = _isDead ? Vector2.Zero : new(
+            SidewardsSpeed * (Input.GetActionStrength(Actions.MoveRight) - Input.GetActionStrength(Actions.MoveLeft)),
+            Input.GetActionStrength(Actions.MoveBackward) != 0 ^ Input.GetActionStrength(Actions.MoveForward) != 0
+                ? -ForwardSpeed * (Input.GetActionStrength(Actions.MoveForward))
+                  + BackwardSpeed * (Input.GetActionStrength(Actions.MoveBackward))
+                : 0
+        );
 
         var rightDir = (-Camera.GlobalBasis.Z).Cross(UpDirection).Normalized();
         var forwardDir = rightDir.Cross(UpDirection).Normalized();
@@ -105,22 +90,27 @@ public partial class PlayerController : CharacterControllerBase {
         if (inputDir != Vector2.Zero) {
             var oldAcc = _headBobAcc;
             _headBobAcc = (_headBobAcc + (float)deltaD) % MathF.Max(HeadBobInterval.X, HeadBobInterval.Y);
-            if (_headBobAcc % HeadBobInterval.Y < oldAcc % HeadBobInterval.Y) AudioManager.PlaySound(Step);
+            if (_headBobAcc % HeadBobInterval.Y < oldAcc % HeadBobInterval.Y) {
+                AudioManager.PlaySound(Step);
+            }
             var intervalHalf = HeadBobInterval / 2f;
-            Camera.Position = HeadBobOffset * (new Vector3(1f, 1f, 1f) -
-                                               (new Vector3(_headBobAcc, _headBobAcc, _headBobAcc) % HeadBobInterval
-                                                - intervalHalf).Abs() / intervalHalf);
+            Camera.Position = HeadBobOffset * (new Vector3(1f, 1f, 1f)
+                                               - (new Vector3(_headBobAcc, _headBobAcc, _headBobAcc) % HeadBobInterval
+                                                  - intervalHalf).Abs() / intervalHalf);
         }
 
         var prevVel = Velocity;
         base._PhysicsProcess(deltaD);
 
-        if (IsOnFloor && prevVel.Y < -5.4f) Kill();
+        if (IsOnFloor && prevVel.Y < -5.4f) {
+            Kill();
+        }
     }
 
     public void Kill() {
-        if (_isDead) return;
+        if (_isDead) { return; }
         _isDead = true;
+        
         Animations.Play("Death");
     }
 
@@ -138,13 +128,10 @@ public partial class PlayerController : CharacterControllerBase {
                 OS.Alert(str, "Runtime Error");
             }
         }
-
         GetTree().Quit();
     }
 
-    public void ResetFogRange() {
-        SetFogRange(FogNear, FogFar);
-    }
+    public void ResetFogRange() => SetFogRange(FogNear, FogFar);
 
     public void SetFogRange(float near, float far) {
         RenderingServer.GlobalShaderParameterSet("fog", new Vector2(near, far));
